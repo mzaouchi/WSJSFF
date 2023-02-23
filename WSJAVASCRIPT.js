289420f6daddb700715d20923a64ec71048cf3cc
mzaouchi <mzaouchi@gmail.com>
2023-02-23 17:01:37 +0100

    first commit

---
+++ WSJAVASCRIPT.js
@@ -0,0 +1,200 @@
+// console.log('Hello Javascript')
+
+// var a = 5
+// var b = 6
+
+// console.log(b)
+
+// var str = "Alaya"
+
+// console.log(str)
+// console.log(typeof(str))
+
+
+// var a = 3
+
+// console.log(a)
+// console.log(typeof(a))
+
+
+// var bool = true
+
+// console.log(bool)
+// console.log(typeof(bool))
+
+
+// var tab = [1,2,3,4]
+
+// console.log(tab)
+// console.log(tab[2])
+// console.log(typeof(tab))
+// console.log(tab[4])
+
+// var student = {name : "Alaya", age : 27,city :"MT"}
+
+// console.log(student)
+// console.log(student.age)
+// console.log(student.name)
+// console.log(Object.keys(student))
+// console.log(Object.values(student))
+// console.log(Object.entries(student))
+
+// var c
+// console.log(typeof(c))
+
+// var c = null
+
+// console.log(typeof(c))
+
+
+// var a = 5
+// var b = 2
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(b**a)
+
+// console.log(a)
+// a++
+// console.log(a)
+// a--
+// console.log(a)
+
+// var name  = "Alaya"
+
+// if(name === "Alaya"){
+//   console.log("Bonjour Alaya")
+// }
+
+// var name = "Hazem"
+
+// if(name == "Hazem"){
+//   console.log("Bonjour Hazem")
+// }else{
+//   console.log("Bonjour user")
+// }
+
+// var age = 20
+
+// if(age <=18){
+//   console.log("Young")
+// }else if(age<=60){
+//   console.log("Adule")
+// }else{
+//   console.log("Old")
+// }
+
+// var  num = 1
+
+// switch(num){
+//   case 1 : console.log("Un")
+//            break 
+//   case 2 : console.log("Deux")
+//            break
+//   default : console.log("Number")
+// }
+
+// // var a = 0
+// // var a = false
+// // var a = NaN
+// // var a = ""
+// // var a = undefined
+// // var a = null
+// var a = "0"
+
+// if(a){
+//   console.log("Hello")
+// }
+
+// var a = 2
+// var b = 7
+// var c = 10
+
+// if (a < b && a > c) {
+//   console.log('a<b')
+// }
+
+// if(a<b || a>c){
+//   console.log('a<b')
+// }
+
+// var num = 2
+
+// if(num % 2 == 0){
+//   console.log("Pair")
+// }else{
+//   console.log("impair")
+// }
+
+
+// function Somme(a,b){
+//   return a+b
+// }
+
+
+// console.log(Somme(2,4))
+
+// function pairimpair(k){
+//     if(k % 2 == 0){
+//       return "Pair"
+//     }else{
+//       return "Impair"
+//     }
+//   }
+  
+//   console.log(pairimpair(2))
+//   console.log(pairimpair(5))
+
+//   for(var i = 0;i<3;i++){
+//     console.log("Mahmoud")
+//   }
+
+//   var tab = [1000,2000,3000]
+
+// for(var i = 0;i<tab.length;i++){
+//   console.log(tab[i])
+// }
+
+// var tab = [1000,2000,3000]
+// var num = 1000
+// var verif = false
+
+// for(var i = 0;i<tab.length;i++){
+//   if(tab[i]== num){
+//     verif = true
+//   } 
+// }
+
+// if(verif == true){
+//   console.log("Mawjoud")
+// }else{
+//   console.log("Mouch mawjoud")
+// }
+
+
+// var tab = [1000, 2000, 3000]
+
+// for(var djeja in tab){
+//   console.log(tab[djeja])
+// }
+
+// for(var mahmoud of tab){
+//     console.log(mahmoud)
+//   } 
+
+var tab = [1000, 2000, 3000]
+var i = 0
+
+// while (i<tab.length) {
+//     console.log(tab[i])
+//     i++
+// }
+
+do {
+    console.log(tab[i])
+    i++
+} while (i<tab.length);
+
